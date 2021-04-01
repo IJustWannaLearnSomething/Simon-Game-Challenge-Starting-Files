d2c77d05f77ff2524461744afb900a68646e21e0
IJustWannaLearnSomething <mohitkik2@gmail.com>
2021-04-01 17:49:19 +0530

    203

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-var userClickedPattern = [];
+var userClickedPattern = []
+var level = 0;
 
-
-
+$(document).keypress(function() {
+  if(gamePattern.length == 0) {
+    nextSequence();
+  }
+});
 
 $(".btn").on("click", function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
-
-
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -19,10 +22,17 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  $("h1").text("Level " + ++level);
 }
-
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
